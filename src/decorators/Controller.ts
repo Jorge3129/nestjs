f5d1fdad48526c192491badb95f3b91450ef2bb5
Heorhii <jorhesancho@gmail.com>
2022-09-04 17:01:54 +0300

    Enable Injection In Controller And Injectable

--- src/decorators/Controller.ts
+++ src/decorators/Controller.ts
@@ -1,19 +1,34 @@
 import metaDataStorage from "../metadata/MetaDataStorage";
+import { ConstructorParam } from "../metadata/types/ConstructorParam";
 
 export const Controller = (path: string = "/"): ClassDecorator => {
   return <T extends Function>(target: T): T | void => {
     const handlers = metaDataStorage.handlers.concat();
+    const injectParams = metaDataStorage.injectParams;
 
-    const params = Reflect.getMetadata("design:paramtypes", target);
+    const paramTypes: Function[] = Reflect.getMetadata(
+      "design:paramtypes",
+      target
+    );
+
+    const constructorParams: ConstructorParam[] = paramTypes.map((type) => ({
+      type,
+      token: type.name,
+    }));
+
+    injectParams.map(({ token, paramIndex }) => {
+      constructorParams[paramIndex].token = token;
+    });
 
     metaDataStorage.controllerMap.set(target.name, {
       target: target as any,
-      constructorParams: params,
+      constructorParams,
       path,
       handlers,
     });
 
     metaDataStorage.handlers = [];
+    metaDataStorage.injectParams = [];
 
     return target;
   };
--- src/decorators/Injectable.ts
+++ src/decorators/Injectable.ts
@@ -1,13 +1,30 @@
 import metaDataStorage from "../metadata/MetaDataStorage";
+import { ConstructorParam } from "../metadata/types/ConstructorParam";
 
 export const Injectable = (): ClassDecorator => {
   return <T extends Function>(target: T): T | void => {
-    const params = Reflect.getMetadata("design:paramtypes", target);
+    const paramTypes: Function[] = Reflect.getMetadata(
+      "design:paramtypes",
+      target
+    );
+
+    const injectParams = metaDataStorage.injectParams;
+
+    const constructorParams: ConstructorParam[] = paramTypes.map((type) => ({
+      type,
+      token: type.name,
+    }));
+
+    injectParams.map(({ token, paramIndex }) => {
+      constructorParams[paramIndex].token = token;
+    });
 
     metaDataStorage.serviceMap.set(target.name, {
-      constructorParams: params ?? [],
+      constructorParams,
       target: target as any,
     });
+
+    metaDataStorage.injectParams = [];
 
     return target;
   };
